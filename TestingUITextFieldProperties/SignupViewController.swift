d2e08efa844d2d70dcde20f390e66ddbea416d0e
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2022-05-01 11:58:26 +0900

    add

---
+++ TestingUITextFieldProperties/SignupViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SignupViewController.swift
+//  TestingUITextFieldProperties
+//
+//  Created by Toshiyana on 2022/05/01.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTexxtField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var mobilePhoneTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
--- TestingUITextFieldPropertiesTests/TestingUITextFieldPropertiesTests.swift
+++ TestingUITextFieldPropertiesTests/TestingUITextFieldPropertiesTests.swift
@@ -10,27 +10,34 @@ import XCTest
 
 class TestingUITextFieldPropertiesTests: XCTestCase {
 
+    var sut: SignupViewController!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        sut = storyboard.instantiateViewController(identifier: "SignupViewController", creator: nil) as? SignupViewController
+        sut.loadViewIfNeeded()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+    }
+    
+    func testEmailTextField_WhenCreated_HasEmailAddressContentTypeSet() throws {
+        let emailTextField = try XCTUnwrap(sut.emailTextField, "Email Address UITextField is not connected")
+        
+        XCTAssertEqual(emailTextField.textContentType, .emailAddress, "Email Address UITextField does not have a Email Addres Content Type set")
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testEmailTextField_WhenCreated_HasEmailKeyboardTypeSet() throws {
+        let emailTextField = try XCTUnwrap(sut.emailTextField, "Email Address UITextField is not connected")
+        
+        XCTAssertEqual(emailTextField.keyboardType, UIKeyboardType.emailAddress, "Email Address UITextField does not have a Email Keyboard Type set")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testPasswordTextField_WhenCreated_IsSecureTextEntryField() throws {
+        let passwordTextField = try XCTUnwrap(sut.passwordTextField, "Password UITextField is not connected")
+        
+        XCTAssertTrue(passwordTextField.isSecureTextEntry, "Password UITextField is not a Secure Text Entry Field")
     }
-
 }
